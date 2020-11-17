2b0b3b4612998b11d935fb7faf30d4e54038e4e7
Ryan Way <ryan.keith.way@gmail.com>
2020-11-16 21:04:39 -0800

    initial working solution

---
+++ test_generate.py
@@ -0,0 +1,42 @@
+import unittest
+from generate import Generator
+
+class GeneratorTest(unittest.TestCase):
+
+    def test_permuatations(self):
+        g = Generator()
+        self.assertEqual(sum(1 for i in g.getOperatorPermutations()), 120)
+
+    def test_opertatorset(self):
+        g = Generator()
+        self.assertTrue(any((i == ['+']*5) for i in g.getOperatorSet()))
+
+    def test_generate_example1(self):
+        g = Generator()
+        #g.generate([50, 2, 6, 4, 10, 7], 300)
+
+    def test_generate_example2(self):
+        g = Generator()
+        #g.generate([25, 7, 10, 2, 1, 6], 175)
+
+    def test_generate_example3(self):
+        g = Generator()
+        #g.generate([25, 26, 10, 3, 2, 1], 311)
+
+    def test_generate_example4(self):
+        g = Generator()
+        g.generate([5, 6, 4, 50, 75, 25], 184)
+
+    def test_permutation_example1(self):
+        g = Generator()
+        self.assertTrue(any((i[0] == "*") for i in g.getOperatorSet()))
+
+    def test_solve_example1(self):
+        g = Generator()
+        nums = [ 50, 6, 4, 10, 7 ]
+        target = 300
+        ops = ["*"]*5
+        perms = [0 for i in range(5)]
+        equation = [j for i in zip(nums[:-1], ops) for j in i] + [nums[-1]]
+        print(g.solve(perms, equation, target))
+
