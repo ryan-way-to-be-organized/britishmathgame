ff4104881f8e7b2f82a772405d49a8ab5eaef254
Ryan Way <ryan.keith.way@gmail.com>
2020-11-17 06:51:46 -0800

    clean up tests, and test_generate.py

--- test_generate.py
+++ test_generate.py
@@ -12,22 +12,6 @@ class GeneratorTest(unittest.TestCase):
         g = Generator()
         self.assertTrue(any((i == ['+']*5) for i in g.getOperatorSet()))
 
-    def test_generate_example1(self):
-        g = Generator()
-        #g.generate([50, 2, 6, 4, 10, 7], 300)
-
-    def test_generate_example2(self):
-        g = Generator()
-        #g.generate([25, 7, 10, 2, 1, 6], 175)
-
-    def test_generate_example3(self):
-        g = Generator()
-        #g.generate([25, 26, 10, 3, 2, 1], 311)
-
-    def test_generate_example4(self):
-        g = Generator()
-        #g.generate([5, 6, 4, 50, 75, 25], 184)
-
     def test_permutation_example1(self):
         g = Generator()
         self.assertTrue(any((i[0] == "*") for i in g.getOperatorSet()))
@@ -43,16 +27,20 @@ class GeneratorTest(unittest.TestCase):
 
     def test_generate_example1(self):
         g = Generator()
-        print(g.findTarget([50, 2, 6, 4, 10, 7], 300), 300)
+        node = g.findTarget([50,2, 6, 4, 10, 7], 300)
+        self.assertEqual(node.eval(), 300)
 
     def test_generate_example2(self):
         g = Generator()
-        print(g.findTarget([25, 7, 10, 2, 1, 6], 175), 175)
+        node = g.findTarget([25, 7, 10, 2, 1, 6], 175)
+        self.assertEqual(node.eval(), 175)
 
     def test_generate_example3(self):
         g = Generator()
-        print(g.findTarget([25, 26, 10, 3, 2, 1], 311), 311)
+        node = g.findTarget([25, 26, 10, 3, 2, 1], 311)
+        self.assertEqual(node.eval(), 311)
 
     def test_generate_example4(self):
         g = Generator()
-        print(g.findTarget([5, 6, 4, 50, 75, 25], 184), 184)
+        node = g.findTarget([5, 6, 4, 50, 75, 25], 184)
+        self.assertEqual(node.eval(), 184)
